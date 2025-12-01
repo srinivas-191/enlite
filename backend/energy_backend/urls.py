d8907ea3c065950a302b6bb8338dafab1a314423
srinivas-191 <srinivasrajoli2002@gmail.com>
2025-12-01 22:37:07 +0530

    gunicornupdated

--- backend/energy_backend/urls.py
+++ backend/energy_backend/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Backend is running!")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("energy_api.urls")),
+    path("", home),  # <-- this handles the root URL
 ]
