4d27b00b4e2d9cd6289ffb0422abfe8877bc7d7e
srinivas-191 <srinivasrajoli2002@gmail.com>
2025-12-01 21:57:36 +0530

    Backend updated with render.yaml

--- backend/energy_backend/settings.py
+++ backend/energy_backend/settings.py
@@ -112,4 +112,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
