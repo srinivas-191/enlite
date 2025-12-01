75d388118cc8eaa4d3031fe7bca923f9b829bbc0
srinivas-191 <srinivasrajoli2002@gmail.com>
2025-12-01 16:41:44 +0530

    first commit

--- frontend/src/pages/PredictPage.jsx
+++ frontend/src/pages/PredictPage.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import html2pdf from "html2pdf.js";
 
-const API_BASE = "https://127.0.0.1:8000/api";
+const API_BASE = "http://127.0.0.1:8000/api";
 
 const SECTION_ORDER = [
   "Building_Type",
